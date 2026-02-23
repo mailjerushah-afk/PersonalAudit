7deee969f886f1605120aa22b17dfd11ddb179f4
Jerusha J <mailjerushah@gmail.com>
2026-02-23 18:44:56 +1100

    feat: add JWTAuthentication (#3)

---
+++ backend/demo/src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,24 @@
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "supersecretkeysupersecretkey";
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
