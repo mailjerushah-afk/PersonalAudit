8e882e366e58601dff18429f17323844ea48bdb4
Jerusha Jobson <jerushajobson@Jerushas-MacBook-Pro.local>
2026-03-04 12:06:59 +1100

    feat: full page setup completed

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api/api";
-import { User } from "../types/User";
+//import type { User } from "../types/User";
+import type { UserRequest } from "../types/UserRequest";
 
 export default function Register() {
 
@@ -13,9 +14,9 @@ export default function Register() {
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const newUser: User = {
-      email,
-      fullName
+    const newUser: UserRequest = {
+        email,
+        fullName
     };
 
     try {
