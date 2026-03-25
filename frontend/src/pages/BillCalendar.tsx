e0a7b8f23648e2c0ba0c030f4ca251ad549096cd
Jerusha Jobson <jerushajobson@Jerushas-MacBook-Pro.local>
2026-03-25 13:37:17 +1100

    feat: code version cleanup

--- frontend/src/pages/BillCalendar.tsx
+++ frontend/src/pages/BillCalendar.tsx
@@ -1,39 +1,3 @@
-// import { useState } from "react";
-// import Calendar from "react-calendar";
-// import { useNavigate } from "react-router-dom";
-// import { styles } from "../styles/GlobalStyles";
-
-// type ValuePiece = Date | null;
-// type Value = ValuePiece | [ValuePiece, ValuePiece];
-
-// export default function BillCalendar() {
-
-//   const navigate = useNavigate();
-
-//   const [date, setDate] = useState<Value>(new Date());
-
-//   return (
-//     <div style={styles.page}>
-//       <div style={styles.container}>
-
-//         <h1 style={styles.title}>Bill Calendar</h1>
-
-//         <button
-//           style={styles.button}
-//           onClick={() => navigate("/dashboard")}
-//         >
-//           Back
-//         </button>
-
-//         <Calendar
-//           value={date}
-//           onChange={(value: Value) => setDate(value)}
-//         />
-
-//       </div>
-//     </div>
-//   );
-// }
 import { useState } from "react";
 import Calendar from "react-calendar";
 import { useNavigate } from "react-router-dom";
