37b6db00a0bcf943371541659fa6fddae18b49e2
Jerusha J <mailjerushah@gmail.com>
2026-04-12 10:23:29 +1000

    feat: add alerts (#32)

---
+++ frontend/src/services/billService.tsx
@@ -0,0 +1,12 @@
+import type { Bill } from "../types/Bill";
+
+const STORAGE_KEY = "bills";
+
+export const getBills = (): Bill[] => {
+  const data = localStorage.getItem(STORAGE_KEY);
+  return data ? JSON.parse(data) : [];
+};
+
+export const saveBills = (bills: Bill[]) => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(bills));
+};
